c506a6fa092376a927f8ecedae891c7e70e2344c
brandontyruspetty <tyrus1974@gmail.com>
2022-12-15 12:58:32 -0800

    create main-view.jsx

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+
+export const MainView = () => {
+  const [movies, setMovies] = useState([
+    { 
+      id: 1, 
+      title: "Le Samourai",
+      image: "https://s3.amazonaws.com/criterion-production/films/9bee98615a85bea1878af49ba320ca40/WvMVqQiUO8W03H1PGJcf0J5s64wNJe_large.jpg",
+      director: "Jean-Pierre Melville"
+    },
+    { 
+      id: 2, 
+      title: "Drive",
+      image: "https://upload.wikimedia.org/wikipedia/en/1/13/Drive2011Poster.jpg",
+      director: "Niocholas Winding Refn"
+    },
+    { 
+      id: 3, 
+      title: "The Third Man",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/The_Third_Man_%281949_American_theatrical_poster%29.jpg/440px-The_Third_Man_%281949_American_theatrical_poster%29.jpg",
+      director: "Carol Reed"
+    }
+  ]);
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>
+  }
+
+  return (
+    <div>
+      {movies.map((movie) => {
+        return <MovieCard movie={movie} />;
+      })}
+    </div>
+  );
+};
