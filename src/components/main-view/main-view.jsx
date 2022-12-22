27d2665c1ec270ef733680c2a929337b98de4b11
brandontyruspetty <tyrus1974@gmail.com>
2022-12-21 19:45:07 -0800

    imported login-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,11 +1,16 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+
 
 
 export const MainView = () => {
 
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  const [user, setUser] = useState(null);
   
   useEffect(() => {
     fetch("https://mynoirmovies.herokuapp.com/movies")
@@ -26,7 +31,7 @@ export const MainView = () => {
     });
   }, []);
 
-  const [selectedMovie, setSelectedMovie] = useState(null);
+  
 
   if (selectedMovie) {
     return (
