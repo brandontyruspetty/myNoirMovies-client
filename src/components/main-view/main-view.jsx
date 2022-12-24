f9faa1004617f4bc79f59951866f09ac18eb9002
brandontyruspetty <tyrus1974@gmail.com>
2022-12-23 16:46:40 -0800

    imported signup view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
+
 
 
 
@@ -35,12 +37,16 @@ export const MainView = () => {
 
   if (!user) {
     return ( 
+      <>
     <LoginView 
       onLogggedIn={(user, token) => {
         setUser(user);
         setToken(token);
       }}
        />
+       or
+       <SignupView />
+       </>
     );
   }
   
