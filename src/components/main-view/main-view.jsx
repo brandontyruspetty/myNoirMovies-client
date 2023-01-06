4db6f0cc3e838d909a3c91d71894359dbeadcf10
brandontyruspetty <tyrus1974@gmail.com>
2023-01-05 17:20:59 -0800

    react bootstrap changes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,12 +1,20 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+//import { LoginView }
+//import { SignupView }
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+
 
 
 export const MainView = () => {
 
   const [movies, setMovies] = useState([]);
-  
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  //const [user, set User]
+
   useEffect(() => {
     fetch("https://mynoirmovies.herokuapp.com/movies")
     .then((response) => response.json())
@@ -26,29 +34,38 @@ export const MainView = () => {
     });
   }, []);
 
-  const [selectedMovie, setSelectedMovie] = useState(null);
-
-  if (selectedMovie) {
-    return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-    );
-  }
-
-  if (movies.length === 0) {
-    return <div>The list is empty!</div>
-  }
-
   return (
-    <div>
-      {movies.map((movie) => {
-         return <MovieCard 
-         key={movie.id}
-         movie={movie}
-         onMovieClick={(newSelectedMovie) => {
-          setSelectedMovie(newSelectedMovie);
-         }} 
-         />
-        })}
-    </div>
+    <Row className="justify-content-md-center">
+       {/* {!user ? ( 
+          <Col md={5}>
+            <LoginView onLoggedIn={(user)} => setUser(user)} />
+            or
+            <SignupView />
+          </Col>
+  )  */}
+  : selectedMovie ? (
+    <Col md={8}>
+      <MovieView
+      movie={selectedMovie} 
+      onBackClick={() => setSelectedMovie(null)} 
+      />
+      </Col>
+  ) : movies.length === 0 ? (
+     <div>The list is empty!</div>
+  ) : (
+    <>
+      {movies.map((movie) => (
+        <Col className="mb-5" key={movie.id} md={3}>
+          <MovieCard 
+            movie={movie}
+            onMovieClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
+            }} 
+          />
+        </Col>
+      ))}
+      </>
+    )}
+    </Row>
   );
 };
