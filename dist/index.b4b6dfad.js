4749f439ae83f77a557e35e777c7280f6ddcbddd
brandontyruspetty <tyrus1974@gmail.com>
2023-01-18 13:14:20 -0800

    dist files

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -46169,6 +46169,7 @@ var _reactRouterDom = require("react-router-dom");
 const NavigationBar = ({ user , onLoggedOut  })=>{
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Navbar), {
         bg: "dark",
+        text: "light",
         expand: "lg",
         children: /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Container), {
             children: [
