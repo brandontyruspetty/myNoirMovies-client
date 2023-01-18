e13fd82b3375b52577443124c105a05dd179b79a
brandontyruspetty <tyrus1974@gmail.com>
2023-01-18 12:30:42 -0800

    added favorite button to movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,32 +1,53 @@
+import { Card } from "react-bootstrap";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import "./movie-view.scss";
 
 export const MovieView = ({ movies }) => {
   console.log(movies);
-  
+
   const { movieId } = useParams();
 
   const movie = movies.find((m) => m.id === movieId);
 
   return (
-    <div>
-      <div>
-        <img className="w-100" src={movie.image} />
-      </div>
-      <div>
-        <span>Title: </span>
-        <span>{movie.title}</span>
-      </div>
-      <div>
-        <span>Director: </span>
-        <span>{movie.director}</span>
-      </div>
-      <Link to={`/`}>
-        <button className="back-button" style={{ cursor: "pointer" }}>
-        Back
-        </button>
-      </Link>
-    </div>
+   <Container>
+    <Row>
+      <Card bg="dark" text="light">
+        <Card.Header>
+          <div className="title-text-center">
+            <span>Title: </span>
+            <span>{movie.title}</span>
+          </div>
+         <Button
+          className="fav-btn"
+          size="sm"
+          variant="secondary"
+          onClick={addFavorite(movie._id)}
+          >
+          Add to Favorites
+          </Button>
+        </Card.Header>
+        <Card.Body>
+        <div>
+          <img className="w-100" 
+          src={movie.image} 
+          />
+        </div>
+        </Card.Body>
+       
+       
+        <div>
+          <span>Director: </span>
+          <span>{movie.director}</span>
+        </div>
+        <Link to={`/`}>
+          <button className="back-button" style={{ cursor: "pointer" }}>
+          Back
+          </button>
+        </Link>
+        </Card>
+        </Row>
+      </Container>
   );
 };
