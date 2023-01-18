45a4de77268e2896d5467155967f4944d34fbb49
brandontyruspetty <tyrus1974@gmail.com>
2023-01-17 16:37:58 -0800

    imports for profileview

---
+++ src/components/profile-view/profile-view.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { Button, Card, CardGroup, Container, Row, Col, Form } from "react-bootstrap";
+
+import UserInfo from "./user-info";
+import FavoriteMovies from "./favorite-movies";
+import UpdateUser from "./update-user";
+import "./profile-view.scss";
+
+export const ProfileView = ({ movies }) => {
+  
+}
