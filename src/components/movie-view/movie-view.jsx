b04dff06d0370a688931118595d1564740b9bd0e
brandontyruspetty <tyrus1974@gmail.com>
2023-01-18 12:49:15 -0800

    update movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,14 +1,34 @@
-import { Card, Button, CardGroup, Row, Container } from "react-bootstrap";
+import { Card, Button, Row, Container } from "react-bootstrap";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import "./movie-view.scss";
 
 export const MovieView = ({ movies }) => {
   console.log(movies);
-
   const { movieId } = useParams();
-
   const movie = movies.find((m) => m.id === movieId);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const token = localStorage.getItem("token");
+
+  const addFavorite = (movieId) => {
+    if (!token) return;
+
+    const url = `https://mynoirmovies.herokuapp.com/users/${storedUser.Username}movies/${movieId}`;
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer $(token)`,
+        },
+      };
+      fetch(url, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((e) => {
+          alert("Something is wrong");
+        });
+  };
 
   return (
    <Container>
