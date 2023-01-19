701daba74842fb8b96c99ebcccd48846d63b261b
brandontyruspetty <tyrus1974@gmail.com>
2023-01-19 14:27:20 -0800

    update nav-bar with username

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -26,8 +26,8 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
               <Nav.Link as={Link} to="/">
                 Home
               </Nav.Link>
-              <Nav.Link as={Link} to="/profile">
-                Profile
+              <Nav.Link as={Link} to="/user">
+                {user.Username}
               </Nav.Link>
               <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
               </>
