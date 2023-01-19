feb6b45c261ab26a03dbc573f8a9e5ae610279d8
brandontyruspetty <tyrus1974@gmail.com>
2023-01-18 17:01:34 -0800

    update favorite movie

---
+++ src/components/profile-view/favorite-movies.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Button, Row, Col, Container, Card, Figure } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./favorite-movies.scss";
+ 
+export const FavoriteMovies = ({ usersFavoriteMovies }) => {
+  const token = localStorage.getItem("token");
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  
+  const removeFavorite = (movieId) => {
+    fetch(`${process.env.API_ENDPOINT}/users/${storedUser.Username}/movies/${movieId}`,
+      {
+        method: "DELETE",
+        headers: { Authorization: `Bearer $(token)`},
+      }
+    ) 
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) {
+          localStorage.setItem("user", JSON.stringify(data));
+          window.open(`/${storedUser.Username}`, "_self");
+        }
+      })
+      .catch((e) => {
+        alert("Something is wrong");
+      });
+  };
+
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Card bg="dark" text="light">
+              <Card.Body>
+                <Col xs={12}>
+                  <Card.Title>
+                    Favorite Movies
+                  </Card.Title>
+                </Col>
+                <Row>
+                  {usersFavoriteMovies.map((movie) => {
+                    return (
+                      <Col
+                        xs={12}
+                        md={6}
+                        lg={3}
+                        key={movie._id}
+                        >
+                          <Card bg="dark" text="light">
+                            <Figure>
+                              <Link to={`/movies/${movies._id}`}>
+                                <Figure.Image
+                                  crossOrigins="anonymous"
+                                  src={movie.image}
+                                  alt={movie.title}
+                                  />
+                                  <Figure.Caption>{movie.title}</Figure.Caption>
+                              </Link>
+                            </Figure>
+                            <Card.Footer>
+                              <Button
+                                className="btn-remove"
+                                variant="secondary"
+                                onClick={() => removeFavorite(movie._id)}
+                                >
+                                  Remove
+                                </Button>
+                            </Card.Footer>
+                          </Card>
+                        </Col>
+                    )
+                  })}
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  };
+
+  export default FavoriteMovies;
