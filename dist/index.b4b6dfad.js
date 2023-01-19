18b451506292dcfe8551745743765b130dc1463a
brandontyruspetty <tyrus1974@gmail.com>
2023-01-19 14:41:48 -0800

    dist files

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -46228,7 +46228,7 @@ const NavigationBar = ({ user , onLoggedOut  })=>{
                                     }, undefined),
                                     /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Nav).Link, {
                                         as: (0, _reactRouterDom.Link),
-                                        to: "/user",
+                                        to: "/users",
                                         children: user.Username
                                     }, void 0, false, {
                                         fileName: "src/components/navigation-bar/navigation-bar.jsx",
